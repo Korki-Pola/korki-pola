a800823d1f12d592ec01789d601a2ede84ebf42b
Pola Gudas <pola.gudas@gmail.com>
2023-06-10 23:09:22 +0200

    zad8

---
+++ 2.-programowanie/zadania/zadanie-8.py
@@ -0,0 +1,6 @@
+val_cal = int(input('Podaj liczbę całkowitą: '))
+val_zp = float(input('Podj liczbę zmiennoprzecinkową: '))
+
+suma = val_cal + val_zp
+
+print(f'Suma {val_cal} i {val_zp} to {suma}.')
