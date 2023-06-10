64e7334f85d2cc1f2bc393d2f9c77c22436ed010
Pola Gudas <pola.gudas@gmail.com>
2023-06-10 17:46:31 +0200

    zad 5

--- 2.-programowanie/zadania/zadanie-5.py
+++ 2.-programowanie/zadania/zadanie-5.py
@@ -3,11 +3,7 @@ value_2 = input('Podaj druga wartość: ')
 value_3 = input('Podaj trzecią wartość: ')
 value_4 = input('Podaj czwartą wartość: ')
 
-len_value_1 = len(value_1)
-len_value_2 = len(value_2)
-len_value_3 = len(value_3)
-len_value_4 = len(value_4)
+longest_value = max(value_1, value_2, value_3, value_4, key=len)
+lenght = len(longest_value)
 
-max_len_val = max(len_value_1, len_value_2, len_value_3, len_value_4)
-
-print(f'Najdłuższa wartość to:{max_len_val}')
+print(f'Najdłuższa wartość to {longest_value} i ma długość {lenght}.')
