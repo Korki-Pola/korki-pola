586fca6e3e5b8d4ed902470f3a49f5f625a66299
Szymonexis <szymonkaszuba3@wp.pl>
2023-06-11 12:15:39 +0200

    correct zadanie 5

--- 2.-programowanie/zadania/zadanie-5.py
+++ 2.-programowanie/zadania/zadanie-5.py
@@ -3,7 +3,7 @@ value_2 = input('Podaj druga wartość: ')
 value_3 = input('Podaj trzecią wartość: ')
 value_4 = input('Podaj czwartą wartość: ')
 
-longest_value = max(value_1, value_2, value_3, value_4, key=len)
+longest_value = max(value_1, value_2, value_3, value_4)
 lenght = len(longest_value)
 
 print(f'Najdłuższa wartość to {longest_value} i ma długość {lenght}.')
