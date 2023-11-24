6bce1079df8feafb9627974e0b378869d8feae50
Szymonexis <szymonkaszuba3@wp.pl>
2023-11-24 20:00:19 +0100

    add zadanie-1-6

---
+++ 1.-algorytmika/struktury-i-algorytmy/zadanie-1-6.py
@@ -0,0 +1,32 @@
+# funckja wydawania reszty metoda zachlanna
+def get_change(money: float, price: float, denomination_table: list[float]) -> list[tuple[int, float]]:
+    # obliczenie wartosci reszty
+    change_value = money - price
+
+    print(f'change_value: {change_value}')
+
+    # posortowanie tablicy nominalow malejaco
+    denomination_table.sort(reverse=True)
+
+    lowest_denomination = denomination_table[len(denomination_table) - 1]
+    change: list[tuple[int, float]] = []
+    index = 0
+    while change_value >= lowest_denomination:
+        denomination = denomination_table[index]
+        amount = int(change_value // denomination)
+
+        if amount != 0:
+            change.append((amount, denomination))
+            change_value = round(change_value - amount * denomination, 2)
+
+        index += 1
+
+    return change
+
+
+nominaly = [200, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1, 0.01, 0.02, 0.05]
+cena = 500.24
+pieniadze = 728
+
+
+print(get_change(pieniadze, cena, nominaly))
